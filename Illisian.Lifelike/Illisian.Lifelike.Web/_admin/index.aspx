2a930ec47ecff19c5b4a74464a9c23ba501dbbdb
Matthew Mckenzie <admin@illisian.com.au>
2012-05-23 01:21:17 +1000

    Updates

--- Illisian.Lifelike/Illisian.Lifelike.Web/_admin/index.aspx
+++ Illisian.Lifelike/Illisian.Lifelike.Web/_admin/index.aspx
@@ -4,5 +4,29 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="body" runat="server">
-    
+    <ext:Desktop ID="dtMain" runat="server">
+        <Modules>
+            <ext:DesktopModule ModuleID="dmContentEditor" AutoRun="true">
+                <Window>
+                    <ext:Window ID="winContent" runat="server" Width="600" Height="400" Title="Content Manager"
+                        Maximizable="true" Minimizable="true" Layout="BorderLayout">
+                        <Items>
+                            <ext:Panel ID="Panel2" runat="server" Title="Items" Region="West" BodyPadding="6"
+                                Collapsible="false" Width="200" />
+                            <ext:Panel ID="Panel4" runat="server" Title="" Region="Center" Collapsible="false" Layout="BorderLayout">
+                                <Items>
+                                    <ext:Panel ID="Panel1" runat="server" Title="" Region="North" BodyPadding="6" Height="75"
+                                        Collapsible="false" />
+                                    <ext:Panel ID="Panel3" runat="server" Title="Details" Region="Center" BodyPadding="6"
+                                        Html="" Collapsible="false" />
+                                </Items>
+                            </ext:Panel>
+                        </Items>
+                    </ext:Window>
+                </Window>
+                <Shortcut Name="Content Manager" SortIndex="3" />
+                <Launcher Text="Content Manager" />
+            </ext:DesktopModule>
+        </Modules>
+    </ext:Desktop>
 </asp:Content>
--- Illisian.Lifelike/Illisian.Lifelike.Web/_admin/index.aspx.designer.cs
+++ Illisian.Lifelike/Illisian.Lifelike.Web/_admin/index.aspx.designer.cs
@@ -11,5 +11,59 @@ namespace Illisian.Lifelike._admin {
     
     
     public partial class index {
+        
+        /// <summary>
+        /// dtMain control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::Ext.Net.Desktop dtMain;
+        
+        /// <summary>
+        /// winContent control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::Ext.Net.Window winContent;
+        
+        /// <summary>
+        /// Panel2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::Ext.Net.Panel Panel2;
+        
+        /// <summary>
+        /// Panel4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::Ext.Net.Panel Panel4;
+        
+        /// <summary>
+        /// Panel1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::Ext.Net.Panel Panel1;
+        
+        /// <summary>
+        /// Panel3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::Ext.Net.Panel Panel3;
     }
 }
