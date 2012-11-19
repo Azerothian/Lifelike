8fa7284f3bfc81b242e539f9e2122c116fb17168
Matthew Mckenzie <admin@illisian.com.au>
2012-11-20 02:35:10 +1000

    started to added dockable components

---
+++ Lifelike/Lifelike.JScript.Admin/Modules/Panels/DockableControl.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Html;
+using System.Text;
+using Lifelike.JScript.Admin.Controls;
+using jQueryApi.UI.Widgets;
+namespace Lifelike.JScript.Admin.Modules.Panels
+{
+	public class DockableControl : Control
+	{
+		private Label _header;
+
+		public DockableControl(string name)
+			: base(name)
+		{
+			_header = new Label("Header");
+			_header.CssClass = "ui-dialog-titlebar ui-widget-header ui-corner-all ui-helper-clearfix";
+			CssClass = "ui-dialog ui-widget ui-widget-content ui-corner-all";
+			AddChild(_header);
+		}
+
+		public override void PreRender()
+		{
+			
+		}
+		public override void PostRender()
+		{
+
+			//jQueryApi.jQuery.FromElement(ControlContainer).
+			base.PostRender();
+		}
+	}
+}
